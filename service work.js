91d9b3dd4a5ae48062ae20e735e4ca11648bbb5b
madigwe21-ops <madigwe21@gmail.com>
2026-03-02 12:39:28 +0100

    update service work.js

--- service work.js
+++ service work.js
@@ -1,18 +1,39 @@
-const CACHE_NAME = "cbt-cache-v2";
+const CACHE_NAME = "micky-cbt-v4";
 
-const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/questions.js",
-  "/script.js",
-  "/style.css"
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./images/maths.png",
+  "./images/physics.png",
+  "./images/chemistry.png",
+  "./images/biology.png",
+  "./images/cbt.png",
+  "./images/about.png"
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME)
-    .then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
